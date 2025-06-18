d807f605c80c839d974435838dc34d724b856053
calebkpai <119488597+calebkpai@users.noreply.github.com>
2025-06-18 13:46:38 -0700

    created git ignore

--- controller_esp/controller.py
+++ controller_esp/controller.py
@@ -1,27 +1,18 @@
-import ujson #JSON configuration
+import espnow #ESP-NOW library
+import network #ESP network library
 from machine import SPI, Pin, ADC #SPI, pin, and ADC reading
 import time #sleep method
 
-spi = SPI(1, baudrate=40000) #using spi0 at 400kHz
-
-#pin 13 MOSI
-#pin 12 MISO
-spi_chipSelect = Pin(4, mode=Pin.OUT, value=1); #arbitrary pin 4
-
 arm_motor_pins = { # usable ADC pins 0, 2, 4, 12, 13, 14, 15, 25, 26, 27, 32, 33, 34, 35, 36, and 39.
-                    "forward_backward" : ADC(Pin(35)),  
-                    "left_right" : ADC(Pin(36)),
-                    "arm_yaw" : ADC(Pin(0)),
-                    "arm_segment1": ADC(Pin(2)),
-                    "arm_segment2": ADC(Pin(4)),
-                    "arm_segment3": ADC(Pin(12)),
-                    "grabber_roll": ADC(Pin(13)),
-                    "grabber_grab": ADC(Pin(14)),
+                    "forward_backward" : ADC(Pin(32)),  
+                    "left_right" : ADC(Pin(33)),
+                    "arm_yaw" : ADC(Pin(34)),
+                    "arm_segment1": ADC(Pin(35)),
+                    "arm_segment2": ADC(Pin(36)),
+                    "arm_segment3": ADC(Pin(37)),
+                    "grabber_roll": ADC(Pin(38)),
+                    "grabber_grab": ADC(Pin(39)),
                     }
-
-#for pin in arm_motor_pins: # sets all ADC pins 
-#   pin.atten(ADC.ATTN_11DB) # sets ADC pin read from 0V-3.3V
-#    pin.width(ADC.WIDTH_10BIT) # sets analog reading to be 10 bits long
 
 movement_vectors = {
                     "forward_backward" : 0.0, # RC car forward & backward movement neg# = back, pos# = for
@@ -33,14 +24,31 @@ movement_vectors = {
                     "grabber_roll":0,  #grabber wrist movement 0-180 (ARBITRARY) 0=left movement, 180=right movement
                     "grabber_grab":0,  #grabber pinch movement 0-90 (ARBITRARY) 0=open, 90=closed 
                     }
-vectorArray = [0,0,0,0,0,0,0,0]
-while True:
+vector_array = [0,0,0,0,0,0,0,0]
+
+# A WLAN interface must be active to send()/recv()
+station = network.WLAN(network.STA_IF)  # Or network.AP_IF
+station.active(True)
+
+# Initialize ESP-NOW
+esp = espnow.ESPNow()
+esp.active(True)
+
+# Define the MAC address of the receiving ESP32 (ESP32 B)
+peer = b'x!\x84\xc68\xb0'
+esp.add_peer(peer)
+
+def read_analog_inputs():
     count = 0
     for control in movement_vectors.keys(): #for all movement vector names, read their voltage level and update the dictionary
-        adcReading = arm_motor_pins[control].read()
-        movement_vectors[control] = adcReading
-        vectorArray[count] = adcReading
+        adc_reading = arm_motor_pins[control].read()
+        movement_vectors[control] = adc_reading
+        vector_array[count] = adc_reading.to_bytes(10,big)
         count = count + 1
-    time.sleep(2)
+            
+while True:
+    read_analog_inputs()
     print("---------------------\n")
-    print(vectorArray)
+    print(vector_array)
+    esp.send(peer, vector_array)
+    time.sleep(2)
